23ecd2a6202dbb5d23a906c46bcc869cd7b9d103
Leonardo Losoviz <leo@getpop.org>
2020-12-09 15:10:02 +0800

    Fixed error for PHP 8

--- src/RoutingManagerTrait.php
+++ src/RoutingManagerTrait.php
@@ -23,10 +23,12 @@ trait RoutingManagerTrait
         /** @var WP_Query */
         $query = $this->query;
         // If we passed query args STANDARD_NATURE, then it's a route
+        // Compare the keys only, because since PHP 8.0, comparing array values
+        // (included in $query->query_vars) throws error
         return !empty(
             array_intersect(
-                $query->query_vars,
-                WPQueries::STANDARD_NATURE
+                array_keys($query->query_vars),
+                array_keys(WPQueries::STANDARD_NATURE)
             )
         );
     }
